8ec272b2dee97f23f1a03222182fed44ede4de93
Evgeni Dobranov <evgeni@debian>
2016-12-23 03:00:37 -0500

    Basic POST

--- server.js
+++ server.js
@@ -37,12 +37,47 @@ app.get('/', function (req, res) {
     res.send('Hello world!');
 })
 
+/*
 app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'mess_token_483') {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error, wrong token');
 })
+*/
+
+app.post('/webhook', function (req, res) {
+        var data = req.body;
+
+        // Make sure this is a page subscription
+        if (data.object === 'page') {
+
+                // Iterate over each entry - there may be multiple if batched
+                data.entry.forEach(function(entry) {
+                        var pageID      = entry.id;
+                        var timeOfEvent = entry.time;
+
+                        // Iterate over each messaging event
+                        entry.messaging.forEach(function(event) {
+                                if (event.message) {
+                                        receivedMessage(event);
+                                } else {
+                                        console.log("Webhook received unknown event: ", event);
+                                }
+                        });
+                });
+        // Assume all went well.
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
+        }
+});
+
+function receivedMessage(event) {
+        // Putting a stub for now, we'll expand it in the following steps
+        console.log("Message data: ", event.message);
+}
 
 // Find the port number, and start the server on that port
 var port = process.env.PORT || 5000
